2cd2eef4299f2d164957f57186e4f0e5f823fd97
Phuoc Dinh <S3878270@rmit.edu.vn>
2021-10-06 19:26:52 +0700

    skewness

--- asm1_cpp/skewness.cpp
+++ asm1_cpp/skewness.cpp
@@ -19,9 +19,9 @@ long variance_dev(int* ptr, int length)
     long answer = 0;
     for (int i = 0; i < length; i++)
     {
-        answer += pow((float(*(ptr + i)) - mean(ptr, length)), 2);
+        answer += pow(*(ptr + i) - mean(ptr, length), 2);
     }
-    return answer / float(length - 1);
+    return answer / (length - 1);
 
 }
 long skewness(int* ptr, int length)
@@ -30,7 +30,7 @@ long skewness(int* ptr, int length)
     for (int i = 0; i < length; i++)
     {
 
-        answer += pow((float(*(ptr + i)) - mean(ptr, length)) / sqrt(variance_dev(ptr, length)), 3);
+        answer += pow((*(ptr + i)) - mean(ptr, length) / sqrt(variance_dev(ptr, length)), 3);
     }
     return answer / length;
 }
