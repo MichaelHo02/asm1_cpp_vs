5cfaa2d41c80c5173d72393b2c14f094d83d8d74
Phuoc Dinh <S3878270@rmit.edu.vn>
2021-10-06 19:25:06 +0700

    skewness

--- asm1_cpp/skewness.cpp
+++ asm1_cpp/skewness.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
 #include <math.h>
-#include "descriptive_statistic.h"
+#include "descriptive_statistics.h"
 using namespace std;
 
-long mean (int *ptr, int length)
+long mean(int* ptr, int length)
 {
-    long answer  = 0;
-    for (int i = 0; i <length;i++)
+    long answer = 0;
+    for (int i = 0; i < length; i++)
     {
-     answer += (*(ptr+i));
+        answer += (*(ptr + i));
     }
     return answer / (length);
 
 }
 
-long variance_dev(int *ptr, int length)
-{
-    long answer  = 0;
-    for (int i = 0; i<length; i++)
-    {
-        answer  += pow((float(*(ptr+i))-mean(ptr,length)),2);
-    }
-    return answer /float(length-1);
-   
-}
-long skewness (int *ptr, int length)
+long variance_dev(int* ptr, int length)
 {
     long answer = 0;
-    for (int i =0; i < length; i++)
+    for (int i = 0; i < length; i++)
+    {
+        answer += pow((float(*(ptr + i)) - mean(ptr, length)), 2);
+    }
+    return answer / float(length - 1);
+
+}
+long skewness(int* ptr, int length)
+{
+    long answer = 0;
+    for (int i = 0; i < length; i++)
     {
 
-         answer  += pow((float(*(ptr+i))-mean(ptr,length))/sqrt(variance_dev(ptr,length)),3);
+        answer += pow((float(*(ptr + i)) - mean(ptr, length)) / sqrt(variance_dev(ptr, length)), 3);
     }
-    return answer/length;
+    return answer / length;
 }
 
 
