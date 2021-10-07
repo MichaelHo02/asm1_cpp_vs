c2e2f50fc175b919060879fe5801b57da9e460f8
MichaelHo02 <67449167+MichaelHo02@users.noreply.github.com>
2021-10-07 15:32:31 +0700

    Update main

--- asm1_cpp/mode.cpp
+++ asm1_cpp/mode.cpp
@@ -1,11 +1,37 @@
 #include "descriptive_statistics.h"
 #include <iostream>
+#include "sort.h"
 
 using namespace std;
 
+struct dictionary
+{
+	int value;
+	int occurent;
+
+	dictionary() {
+		value = 0;
+		occurent = 0;
+	}
+
+	dictionary(int value, int occurent) {
+		this->value = value;
+		this->occurent = occurent;
+	}
+};
 
 void mode(int* arr, int size) {
-	//for (int i = 0; i < size; ++i) {
-	//	cout << arr[i] << endl;
+	dictionary * mode_value;
+	dictionary current = dictionary(arr[0], 1);
+
+	//for (int i = 1; i < size; ++i) {
+	//	if (arr[i] == current.value) {
+	//		current.value++;
+	//	}
+	//	else {
+	//		if (mode_value->occurent < current.occurent) {
+
+	//		}
+	//	}
 	//}
 }
